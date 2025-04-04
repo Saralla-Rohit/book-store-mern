5606ab6b380c8c250d3cb0ce3fecbee838028c01
Saralla-Rohit <rohitsarla69@gmail.com>
2025-04-04 12:53:29 +0530

    Initial commit of Book Store MERN project

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://rohitsarla69:rohitsarla123@cluster0.iq9va.mongodb.net/backend?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
